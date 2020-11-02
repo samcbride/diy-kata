9d731adf4bbae880ab586552abb4a236558829e4
Shona McBride <mcbride.shona@gmail.com>
2020-11-02 20:35:16 +0000

    fixing kata 6 so it works with one name

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -2,6 +2,7 @@ const { joinNames } = require("../src");
 
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name: 'Bart'}])).toEqual('Bart')
     expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toEqual('Bart, Lisa & Maggie')
     expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}, {name: 'Homer'}])).toEqual('Bart, Lisa, Maggie & Homer')
   });

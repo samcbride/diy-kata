b1fa137a7e30c33780cf25463e6c402743a90ce5
Shona McBride <mcbride.shona@gmail.com>
2020-11-02 20:36:02 +0000

    adding the second file I forgot to add

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,8 +1,11 @@
 const joinNames = namesObj => {
     let array = namesObj.map(a => a.name);
+    if (namesObj.length === 1) {
+        return namesObj[0].name;
+    } else {
     let newArray = array.slice(0, array.length - 1)
                         .join(', ');
     let lastWord = array.slice(array.length - 1, array.length);
     return newArray + ' & ' + lastWord;
-};
+}};
 module.exports = joinNames;
